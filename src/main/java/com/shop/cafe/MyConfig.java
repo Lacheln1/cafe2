edf92a1425b0f19cab869825569e6be43b302427
Lacheln <psionict@naver.com>
2025-03-06 19:55:43 +0900

    login2

---
+++ src/main/java/com/shop/cafe/MyConfig.java
@@ -0,0 +1,19 @@
+package com.shop.cafe;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyConfig implements WebMvcConfigurer {
+	
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**")
+//		.allowedOrigins("http://1227.0.0.1:5500/")
+//		.allowedMethods("*")
+//		.allowedHeaders("*")
+//		.allowCredentials(true);
+//	}
+
+}
