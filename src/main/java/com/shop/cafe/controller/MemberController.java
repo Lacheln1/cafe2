f62a7f8aef9d9f1150974456e88f9e39fbe20f60
Lacheln <psionict@naver.com>
2025-03-06 19:53:16 +0900

    login2 임시

--- src/main/java/com/shop/cafe/controller/MemberController.java
+++ src/main/java/com/shop/cafe/controller/MemberController.java
@@ -12,13 +12,35 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shop.cafe.dto.Member;
 import com.shop.cafe.service.MemberService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @RestController
-@CrossOrigin("http://127.0.0.1:5500")
+//@CrossOrigin("http://127.0.0.1:5500")
 public class MemberController {
 	
 	@Autowired
 	MemberService memberService;
 	
+	@PostMapping("login")
+	public Map<String, String> login(@RequestBody Member m , HttpServletRequest request) {
+		Map<String,String> responseData = new HashMap();
+		try {
+			m=memberService.login(m);
+			if(m!=null) {
+				HttpSession session = request.getSession();
+				System.out.println(session.getId());
+				session.setAttribute("member", m);
+				responseData.put("msg", "ok");
+			} else {
+				responseData.put("msg", "다시 로그인 해주세요");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			responseData.put("msg", "다시 로그인 해주세요");
+		}
+		return responseData;
+	}
 	
 	@PostMapping("insertMember")
 	public Map<String, String> insertMember(@RequestBody Member m) {//body에 있는 내용을 member객체에 똑같이 넣어달라는 뜻 member엔 email,pwd,nickname이 있음
